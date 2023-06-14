52a5b9df547df0939d11b1361d72f5ce5c6e6642
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-14 13:40:34 +0300

    done Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,27 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (11 - (-10)) + (-10), 2);
+            //matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
