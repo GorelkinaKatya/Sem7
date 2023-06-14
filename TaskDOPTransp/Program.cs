1b63f444d3dc51744c3512a6861268d684c1d1a0
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-14 20:32:48 +0300

    done Task DOP transp

---
+++ TaskDOPTransp/Program.cs
@@ -0,0 +1,43 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+
+        Console.WriteLine();
+    }
+}
+
+void MatrixTransposition(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0) / 2; i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - 1 - i, j];
+            matrix[matrix.GetLength(0) - 1 - i, j] = temp;
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальная матрица");
+PrintMatrix(matrix);
+MatrixTransposition(matrix);
+Console.WriteLine("Транспонированная матрица");
+PrintMatrix(matrix);
